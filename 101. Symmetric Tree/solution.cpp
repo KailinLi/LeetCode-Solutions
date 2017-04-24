c767b72744b46f2b97b6c71a5dd7fb3e935a2558
Kailin Li <likailinhust@163.com>
2017-04-24 09:32:50 +0800

    04/24/2017

--- 101. Symmetric Tree/solution.cpp
+++ 101. Symmetric Tree/solution.cpp
@@ -10,12 +10,9 @@
 class Solution {
 public:
     bool judgement(TreeNode* left, TreeNode* right) {
-        if (!left && !right) return true;
-        else if (!left || !right) return false;
-        return left->val==right->val && ((left->left && right->right)?left->left->val==right->right->val:(!left->left && !right->right)?true:false) && ((left->right && right->left)?left->right->val==right->left->val:(!left->right && !right->left)?true:false) && judgement(left->left, right->right) && judgement(left->right, right->left);
+        return (!left && !right)? true:(!left || !right)? false:left->val==right->val && ((left->left && right->right)?left->left->val==right->right->val:(!left->left && !right->right)?true:false) && ((left->right && right->left)?left->right->val==right->left->val:(!left->right && !right->left)?true:false) && judgement(left->left, right->right) && judgement(left->right, right->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if (!root) return true;
-        return judgement(root->left, root->right);
+        return (!root)? true: judgement(root->left, root->right);
     }
 };
