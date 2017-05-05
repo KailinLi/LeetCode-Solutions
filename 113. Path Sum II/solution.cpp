535ca4717af7b5e4c290483c909651d73baf8bc0
Kailin Li <likailinhust@gmail.com>
2017-05-05 16:12:55 +0800

    05/05/2017

--- 113. Path Sum II/solution.cpp
+++ 113. Path Sum II/solution.cpp
@@ -11,12 +11,13 @@ class Solution {
 private:
     vector<vector<int>> res;
 public:
-    void pSum(vector<int> path, TreeNode* root, int sum) {
+    void pSum(vector<int> &path, TreeNode* root, int sum) {
         if (!root) return;
         path.push_back(root->val);
         if (!root->left && !root->right && !(sum - root->val)) res.push_back(path);
         pSum(path, root->left, sum - root->val);
         pSum(path, root->right, sum - root->val);
+        path.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
         vector<int>p;
