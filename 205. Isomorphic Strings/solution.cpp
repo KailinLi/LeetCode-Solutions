a891e68aea20566128fc6c1cc75ad100ae1a002d
Kailin Li <likailinhust@gmail.com>
2017-07-23 16:33:06 +0800

    07/23/2017

---
+++ 205. Isomorphic Strings/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int>s1(256, 0);
+        vector<int>s2(256, 0);
+        for(size_t i = 0; i < s.size(); ++i) {
+            if (s1[s[i]] != s2[t[i]]) return false;
+            s1[s[i]] = i + 1;
+            s2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
